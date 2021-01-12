31da1f45c82d41bd08639f9f84f7939d65ddbe0e
Logan <lruzzier20@stuy.edu>
2021-01-12 12:07:47 -0500

    added length()

--- Radix.java
+++ Radix.java
@@ -3,4 +3,9 @@ public class Radix{
   public static int nth(int n, int col){
     return (int)(((n*1.0)/(Math.pow(10.0, col)))%10);
   }
+
+  public static int length(int n){
+    String ans = Integer.toString(n);
+    return ans.length();
+  }
 }
