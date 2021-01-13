b4c8e6139b0acd9c4cfb7649a151f40c0c19f651
Logan <lruzzier20@stuy.edu>
2021-01-13 18:50:14 -0500

    minor change to length() and some other stuff

--- Radix.java
+++ Radix.java
@@ -6,6 +6,7 @@ public class Radix{
 
   public static int length(int n){
     String ans = Integer.toString(n);
+    if(n<0){return ans.length()-1;}
     return ans.length();
   }
 
@@ -34,15 +35,38 @@ public class Radix{
       }
         dumpster.extend(master);
       for(int f=0;f<bucket.length;f++){
-        System.out.println("bucket " + f + " is:" + bucket[f].toString());
         }
       for(int j=0;j<bucket.length;j++){
         master.extend(bucket[j]);
       }
-      System.out.println(master);
-      System.out.println(largest+"");
-      System.out.println();
     }
+    data=master;
   }
 
+  public static void radixSort(SortableLinkedList data){
+    int largest=1;
+    SortableLinkedList dumpster = new SortableLinkedList();
+    SortableLinkedList master = data;
+    SortableLinkedList[] bucket = new SortableLinkedList[10];
+    for(int m=0;m<bucket.length;m++){
+      bucket[m]= new SortableLinkedList();
+    }
+
+    for(int q=0;q<master.size();q++){
+      if(length(master.get(q))>largest){largest=length(master.get(q));}
+    }
+
+    for(int i=0;i<largest;i++){
+      for(int l=0;l<master.size();l++){
+        bucket[nth(master.get(l), i)].add(master.get(l));
+      }
+        dumpster.extend(master);
+      for(int f=0;f<bucket.length;f++){
+        }
+      for(int j=0;j<bucket.length;j++){
+        master.extend(bucket[j]);
+      }
+    }
+    data=master;
+  }
 }
